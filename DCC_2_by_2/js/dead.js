ea77e60d8f88ea5e12ae72ad2ebec6cc03dab026
Eric Rinkus <ejrinkus@gmail.com>
2020-05-08 22:26:54 -0700

    Add initiative support, and a simple token support (a bar keeping track of the number of living characters)

--- DCC_2_by_2/js/dead.js
+++ DCC_2_by_2/js/dead.js
@@ -1,15 +1,24 @@
 
   function deadHandlers(charI) {
     on(`clicked:dead_${charI}`, function() {
-      getAttrs([`dead_${charI}`], function(values) {
+      getAttrs([`dead_1`, `dead_2`, `dead_3`, `dead_4`, `total_living`], function(values) {
+        // Figure out the new state for charI, since that's the one that changed.
         var val = "";
         if (values[`dead_${charI}`] == "alive") {
           val = "dead";
         } else {
           val = "alive";
         }
+        // Then figure out the total count of living characters.
+        var count = 0;
+        if (val == "alive") count = 1;
+        for (var c = 1; c <= 4; c++) {
+          if (c == charI) continue;
+          if (values[`dead_${c}`] == "alive") count += 1;
+        }
         setAttrs({
-          [`dead_${charI}`]: val
+          [`dead_${charI}`]: val,
+          [`total_living`]: count
         });
       });
     });
