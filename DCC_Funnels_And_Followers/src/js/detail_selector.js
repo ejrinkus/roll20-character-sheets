a73f36b4fec19e1ba75f2ec28cfb36e3c8f33ee6
Eric Rinkus <ejrinkus@gmail.com>
2020-06-28 00:55:59 -0700

    Finally got the button listeners working, thanks to a lovely member of the community.  It seems that the button names have to be lower case if they're in a repeated section.

--- DCC_Funnels_And_Followers/src/js/detail_selector.js
+++ DCC_Funnels_And_Followers/src/js/detail_selector.js
@@ -1,55 +1,12 @@
 
-  on("clicked:repeating_heroes:selectLevel0", function() {
-    console.log("Clicked Level 0");
-    setAttrs({
-      repeating_heroes_selectLevel0: true,
-      repeating_heroes_selectInventory: false,
-      repeating_heroes_selectStatus: false,
-      repeating_heroes_selectAbilities: false,
-      repeating_heroes_selectSpellcasting: false
-    });
-  });
-
-  on("clicked:repeating_heroes:selectInventory", function() {
-    console.log("Clicked Inventory");
-    setAttrs({
-      repeating_heroes_selectLevel0: false,
-      repeating_heroes_selectInventory: true,
-      repeating_heroes_selectStatus: false,
-      repeating_heroes_selectAbilities: false,
-      repeating_heroes_selectSpellcasting: false
-    });
-  });
-
-  on("clicked:repeating_heroes:selectStatus", function() {
-    console.log("Clicked Status");
-    setAttrs({
-      repeating_heroes_selectLevel0: false,
-      repeating_heroes_selectInventory: false,
-      repeating_heroes_selectStatus: true,
-      repeating_heroes_selectAbilities: false,
-      repeating_heroes_selectSpellcasting: false
-    });
-  });
-
-  on("clicked:repeating_heroes:selectAbilities", function() {
-    console.log("Clicked Abilities");
-    setAttrs({
-      repeating_heroes_selectLevel0: false,
-      repeating_heroes_selectInventory: false,
-      repeating_heroes_selectStatus: false,
-      repeating_heroes_selectAbilities: true,
-      repeating_heroes_selectSpellcasting: false
-    });
-  });
-
-  on("clicked:repeating_heroes:selectSpellcasting", function() {
-    console.log("Clicked Spellcasting");
-    setAttrs({
-      repeating_heroes_selectLevel0: false,
-      repeating_heroes_selectInventory: false,
-      repeating_heroes_selectStatus: false,
-      repeating_heroes_selectAbilities: false,
-      repeating_heroes_selectSpellcasting: true
-    });
+  const fields = ['selectlevel0', 'selectinventory', 'selectstatus', 'selectabilities', 'selectspellcasting'];
+  on(fields.map(f => `clicked:repeating_heroes:${f}`).join(' '), function(eventInfo) {
+      const trigger = eventInfo.sourceAttribute.split('_');
+      const thisfield = trigger[3];
+      const output = {};
+      fields.forEach(f => {
+          output['repeating_heroes_' + f] = false;
+      });
+      output['repeating_heroes_' + thisfield] = true;
+      setAttrs(output);
   });
